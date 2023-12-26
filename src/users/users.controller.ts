243b6862e8b98d7c3abb2465e47db36842020d67
safouene <safouene-abbachi@hotmail.fr>
2023-12-26 16:16:58 +0100

    feat: users providers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,31 +8,47 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
-
+import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
   //NOTE : always define static routes before dynamic routes
   @Get()
+  //this is a Query parameter
   findAll(@Query('role') role?: 'ADMIN' | 'INTERN' | 'ENGINEER') {
-    return [];
+    return this.usersService.findAll(role);
   }
   //The :id is a parameter of a route.
+  //this is a route parameter
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns a #${id} user`;
+    return this.usersService.findOne(+id);
   }
   @Post()
-  create(@Body() user: any) {
-    return user;
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.create(user);
   }
   @Patch(':id')
-  update(@Param('id') id: string, @Body() userUpdate: any) {
-    return `This action updates a #${id} user with ${JSON.stringify(
-      userUpdate,
-    )}`;
+  update(
+    @Param('id') id: string,
+    @Body()
+    userUpdate: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.update(+id, userUpdate);
   }
   @Delete(':id')
   delete(@Param('id') id: string) {
-    return `This action removes a #${id} user`;
+    return this.usersService.delete(+id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,77 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'John',
+      email: 'john@gmail.com',
+      role: 'ADMIN',
+    },
+    {
+      id: 2,
+      name: 'Doe',
+      email: 'doe@gmail.com',
+      role: 'ENGINEER',
+    },
+    {
+      id: 3,
+      name: 'Jane',
+      email: 'jane@gmail.com',
+      role: 'INTERN',
+    },
+    {
+      id: 4,
+      name: 'Smith',
+      email: 'smith@gmail.com',
+      role: 'ADMIN',
+    },
+  ];
+
+  findAll(role?: 'ADMIN' | 'INTERN' | 'ENGINEER') {
+    console.log('role', role);
+    if (role) {
+      return this.users.filter((user) => user.role === role);
+    }
+    return this.users;
+  }
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    return user;
+  }
+  create(user: {
+    name: string;
+    email: string;
+    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+  }) {
+    //NOTE : this way we can not find users with the same id
+    const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+    this.users.push({
+      id: usersByHighestId[0].id + 1,
+      ...user,
+    });
+    return user;
+  }
+  update(
+    id: number,
+    userUpdate: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    this.users[userIndex] = {
+      ...this.users[userIndex],
+      ...userUpdate,
+    };
+    return this.users[userIndex];
+  }
+  delete(id: number) {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    const user = this.users[userIndex];
+    this.users.splice(userIndex, 1);
+    return user;
+  }
+}
